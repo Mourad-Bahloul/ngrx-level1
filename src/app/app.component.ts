0ae17265ec76d2da782e120c288b79de65d79827
Mourad BAHLOUL <mourad.bahloul@gmail.com>
2020-11-26 03:30:00 +0100

    very basic ngrx as in the beginning of text-align: center;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
+
+interface AppState {
+  message: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngrx-fire';
+
+  message$: Observable<string>
+
+  constructor(private store: Store<AppState>) {
+    this.message$ = this.store.select('message')
+  }
+
+  spanishMessage() {
+    this.store.dispatch({type: 'SPANISH'})
+  }
+  frenchMessage() {
+    this.store.dispatch({type: 'FRENCH'})
+  }
+
+  defaultMessage() {
+    this.store.dispatch({type: 'ENGLISH'})
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
+import { simpleReducer } from './simple.reducer';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ message: simpleReducer }),
+    StoreDevtoolsModule.instrument({ maxAge: 25 })
   ],
   providers: [],
   bootstrap: [AppComponent]
